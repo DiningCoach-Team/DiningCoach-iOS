fd3d080a12eb2eeabe0c518ca394b2c42e1bbf8d
HundredCleanWater <maxmp9904@gmail.com>
2023-05-08 22:08:45 +0900

    Initial commit

---
+++ DiningCoach/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DiningCoach
+//
+//  Created by 백정수 on 2023/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DiningCoach/DiningCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiningCoachApp.swift
+//  DiningCoach
+//
+//  Created by 백정수 on 2023/05/08.
+//
+
+import SwiftUI
+
+@main
+struct DiningCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
