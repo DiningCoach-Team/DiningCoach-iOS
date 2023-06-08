47846f1fc707d57a20a8f363b04681363d55d2c2
Chamsol Kim <kcsol1005@gmail.com>
2023-06-09 05:47:54 +0900

    Implement [TDH-10] Color에 asset에서 정의한 primary color를 사용할 수 있는 property 추가

---
+++ DiningCoach/Resources/Color/Color+Primary.swift
@@ -0,0 +1,51 @@
+//
+//  Color+Primary.swift
+//  DiningCoach
+//
+//  Created by chamsol kim on 2023/06/09.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    static var primary50: Color {
+        Color("primary-050")
+    }
+    
+    static var primary100: Color {
+        Color("primary-100")
+    }
+    
+    static var primary200: Color {
+        Color("primary-200")
+    }
+    
+    static var primary300: Color {
+        Color("primary-300")
+    }
+    
+    static var primary400: Color {
+        Color("primary-400")
+    }
+    
+    static var primary500: Color {
+        Color("primary-500")
+    }
+    
+    static var primary600: Color {
+        Color("primary-600")
+    }
+    
+    static var primary700: Color {
+        Color("primary-700")
+    }
+    
+    static var primary800: Color {
+        Color("primary-800")
+    }
+    
+    static var primary900: Color {
+        Color("primary-900")
+    }
+}
