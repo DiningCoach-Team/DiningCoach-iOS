396cf631595ff5718eedc8194de934bf0179f8a9
hsuuuk <hsuuuk.dev@gmail.com>
2023-06-30 17:23:59 +0900

    Implement [TDH-29] TermsAgreementView 추가

---
+++ DiningCoach/Sources/NicknameInputView.swift
@@ -0,0 +1,8 @@
+//
+//  NicknameInputView.swift
+//  DiningCoach
+//
+//  Created by 심현석 on 2023/06/30.
+//
+
+import Foundation
---
+++ DiningCoach/Sources/SigninCompleteView.swift
@@ -0,0 +1,8 @@
+//
+//  SigninCompleteView.swift
+//  DiningCoach
+//
+//  Created by 심현석 on 2023/06/30.
+//
+
+import Foundation
