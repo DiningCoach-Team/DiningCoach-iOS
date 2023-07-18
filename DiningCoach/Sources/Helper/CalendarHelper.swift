119aeab3c912ba7d78f71d66c206c7e5ba46c126
SeongHwan-Seo <seosh5704@gmail.com>
2023-07-18 12:24:42 +0900

    Implement [TDH-18] 커스텀 캘린더 헬퍼 추가

--- DiningCoach/Sources/Helper/CalendarHelper.swift
+++ DiningCoach/Sources/Helper/CalendarHelper.swift
@@ -2,7 +2,64 @@
 //  CalendarHelper.swift
 //  DiningCoach
 //
-//  Created by SHSEO on 2023/07/14.
+//  Created by SHSEO on 2023/07/06.
 //
 
 import Foundation
+
+class CalendarHelper
+{
+    let calendar = Calendar.current
+    let dateFormatter = DateFormatter()
+    
+    func monthYearString(_ date: Date) -> String
+    {
+        dateFormatter.dateFormat = "YYYY년 M월"
+        return dateFormatter.string(from: date)
+    }
+    
+    func plusMonth(_ date: Date) -> Date
+    {
+        return calendar.date(byAdding: .month, value: 1, to: date)!
+    }
+    
+    func minusMonth(_ date: Date) -> Date
+    {
+        return calendar.date(byAdding: .month, value: -1, to: date)!
+    }
+    
+    func plusYear(_ date: Date) -> Date
+    {
+        return calendar.date(byAdding: .year, value: 1, to: date)!
+    }
+    
+    func minusYear(_ date: Date) -> Date
+    {
+        return calendar.date(byAdding: .year, value: -1, to: date)!
+    }
+    
+    func daysInMonth(_ date: Date) -> Int
+    {
+        let range = calendar.range(of: .day, in: .month, for: date)!
+        return range.count
+    }
+    
+    func dayOfMonth(_ date: Date) -> Int
+    {
+        let components = calendar.dateComponents([.day], from: date)
+        return components.day!
+    }
+    
+    func firstOfMonth(_ date: Date) -> Date
+    {
+        let components = calendar.dateComponents([.year, .month], from: date)
+        return calendar.date(from: components)!
+    }
+    
+    func weekDay(_ date: Date) -> Int
+    {
+        let components = calendar.dateComponents([.weekday], from: date)
+        return components.weekday! - 1
+    }
+    
+}
